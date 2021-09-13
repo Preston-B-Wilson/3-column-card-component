feb095e1dfd51308d86be6e335ce2044805b7d0b
YoSoyPan <45904881+yosoypan@users.noreply.github.com>
2021-09-12 20:14:50 -0500

    Finished layout and responsive

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,96 @@
-import './App.css';
+import { useState, useEffect } from 'react';
 import Card from './components/Card'
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
+
+const SizeController = () => {
+  const { height, width } = useWindowDimensions();
+
+  if (width>970) {
+    return (
+      <div>
+        <Card 
+          description1={"\
+            Choose a sedan for its \n \
+            affordability and excellent \n \
+            fuel economy. Ideal for \n \
+            cruising in the city or on \n \
+            your next road trip. \
+            "}
+          description2={"\
+            Take an SUV for its spacious \n \
+            interior, power, and \n \
+            versatility. Perfect for your \n \
+            next family vacation and \n \
+            off-road adventures. \
+            "}
+          description3={"\
+            Cruise in the best car brands \n \
+            without the bloated prices. \n \
+            Enjoy the enhanced comfort \n \
+            of a luxury rental and arrive \n \
+            in style. \
+            "}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Card 
+          description1={"\
+            Choose a sedan for its \n \
+            affordability and excellent fuel \n \
+            economy. Ideal for cruising in \n \
+            the city or on your next road \n \
+            trip. \
+            "}
+          description2={"\
+            Take an SUV for its spacious \n \
+            interior, power, and versatility. \n \
+            Perfect for your next family \n \
+            vacation and off-road \n \
+            adventures. \
+            "}
+          description3={"\
+            Cruise in the best car brands \n \
+            without the bloated prices. \n \
+            Enjoy the enhanced comfort of \n \
+            a luxury rental and arrive in \n \
+            style. \
+            "}
+        />
+      </div>
+    );
+  }
+};
 
 function App() {
   return (
     <div className="App">
       <div className="root-wrapper">
-        <Card/>
+        <SizeController/>
       </div>
     </div>
   );
--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,45 +1,24 @@
 import React from 'react';
 
-function Card() {
+function Card(props) {
     return (
         <div className="main-container">
             <div className="content-container container-1">
                 <img src="./images/icon-sedans.svg"></img>
                 <h1>SEDANS</h1>
-                <p>{"\
-                Choose a sedan for its \n \
-                affordability and excellent \n \
-                fuel economy. Ideal for \n \
-                cruising in the city or on \n \
-                your next road trip. \
-                "}
-                </p>
+                <p>{props.description1}</p>
                 <button id="button-1" className="learn-more-button">Learn More</button>
             </div>
             <div className="content-container container-2">
                 <img src="./images/icon-suvs.svg"></img>
                 <h1>SUVS</h1>
-                <p>{"\
-                Take an SUV for its spacious \n \
-                interior, power, and \n \
-                versatility, Perfect for your \n \
-                next family vacation and \n \
-                off-road adventures. \
-                "}
-                </p>
+                <p>{props.description2}</p>
                 <button id="button-2" className="learn-more-button">Learn More</button>
             </div>
             <div className="content-container container-3">
                 <img src="./images/icon-luxury.svg"></img>
                 <h1>LUXURY</h1>
-                <p>{"\
-                Cruise in the best car brands \n \
-                without the bloated prices. \n \
-                Enjoy the enhanced comfort \n \
-                of a luxury rental and arrive \n \
-                in style. \
-                "}
-                </p>
+                <p>{props.description3}</p>
                 <button id="button-3" className="learn-more-button">Learn More</button>
             </div>
         </div>
